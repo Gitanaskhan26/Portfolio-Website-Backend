444fc718f3c945c1da0c628ff9b5feebb751dad6
Gitanaskhan26 <87142370+Gitanaskhan26@users.noreply.github.com>
2025-03-19 10:51:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,9 +23,9 @@ app.get("/", (req, res) => {
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-  res.status(404).json({ message: "🔍 API route not found" });
+  res.status(404).json({ message: "API route not found" });
 });
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
